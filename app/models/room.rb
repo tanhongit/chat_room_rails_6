5fd47553f92ac6978a6944a7bff7f1d9d3e45dd9
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-09 19:07:52 +0700

    Delete all

--- app/models/room.rb
+++
@@ -1,4 +0,0 @@
-class Room < ApplicationRecord
-    has_many :room_messages, dependent: :destroy,
-                         inverse_of: :room
-end
--- app/models/room_message.rb
+++
@@ -1,4 +0,0 @@
-class RoomMessage < ApplicationRecord
-  belongs_to :user
-belongs_to :room, inverse_of: :room_messages
-end
