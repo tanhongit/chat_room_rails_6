7bec051a6cd62ad4e443c1e69a2b34cbf25c4fcf
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-09 18:18:18 +0700

    index room

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :room_messages
+  resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root controller: :rooms, action: :index
+
+  resources :room_messages
+  resources :rooms
 end
