b7e566a65fd68c8b1c5495674574db66fe140e60
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-09 22:14:07 +0700

    chat room load

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,5 +2,8 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     room = Room.find params[:room]
     stream_for room
+
+    # or
+    # stream_from "room_#{params[:room]}"
   end
 end
